218a8b4ac45379c9d280f3fb03dc57d94c7ae000
Emma <64107425+emmadebarros@users.noreply.github.com>
2021-09-07 18:17:18 -0400

    Add DatingApp Project

---
+++ DatingApp/DatingApp/Profile.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace DatingApp
+{
+    //Custom Exception
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string msg) : base(msg) { }
+        public InvalidDataException(string msg, Exception inner) : base(msg, inner) { }
+    }
+
+    public class Profile
+    {
+        //FIELDS (storage)
+
+        private string _name;
+
+        private int _age;
+
+        private EPronouns _pronouns;
+
+        private string _city;
+
+        private string _country;
+
+        private string _description;
+
+        //ENUM
+
+        //FIXME: add 'Other' with possibility to enter other pronoun if selected
+        //FIXME: should display 'HE/HIM', 'She/Her', 'They/Them'
+        public enum EPronouns { He, She, They }
+
+        //PROPERTIES
+
+        //_name
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z]+$"))
+                {
+                    throw new InvalidDataException("Name must contain uppercase and lowercase letters only.");
+                }
+                _name = value;
+            }
+        }
+
+        //_age
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value < 1 || value > 150)
+                {
+                    throw new InvalidDataException("Age must be between 1 and 150.");
+                }
+                _age = value;
+            }
+        }
+
+        //_pronouns
+        public EPronouns Pronouns
+        {
+            get
+            {
+                return _pronouns;
+            }
+
+            set
+            {
+                _pronouns = value;
+            }
+        }
+
+        //_city
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z]+$"))
+                {
+                    throw new InvalidDataException("City must contain uppercase and lowercase letters only.");
+                }
+                _city = value;
+            }
+        }
+
+        //_country
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z]+$"))
+                {
+                    throw new InvalidDataException("Country must contain uppercase and lowercase letters only.");
+                }
+                _country = value;
+            }
+        }
+
+        //_description
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+
+            set
+            {
+                //FIXME: check for curses
+                if (!Regex.IsMatch(value, @"^[a-zA-Z0-9b ()'.-]{1,300}$"))
+                {
+                    throw new InvalidDataException("Description must be 1 to 300 characters long and only contain uppercase letters, lowercase letters, digits 0-9 and special characters like '().-' .");
+                }
+                _description = value;
+            }
+        }
+
+        //CONSTRUCTOR(s)
+
+        public Profile(string name, int age, string pronouns, string city, string country, string description)
+        {
+            try
+            {
+                Name = name; //InvalidDateException from setter
+                Age = age; //InvalidDateException from setter
+                Pronouns = (EPronouns)Enum.Parse(typeof(EPronouns), pronouns, true); // exc from parsing
+                City = city; //InvalidDateException from setter
+                Country = country; //InvalidDateException from setter
+                Description = description; //InvalidDateException from setter
+            }
+            catch (Exception exc) when (exc is ArgumentException || exc is ArgumentNullException || exc is OverflowException)
+            {
+                throw new InvalidDataException("Pronoun not found.");
+            }
+        }
+
+        //OVERRIDES
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\nAge: {1}\nPronouns: {2}\nCity & Country: {3}, {4}\nDescription: {5}", this.Name, this.Age, this.Pronouns.ToString().Equals("He") ? "He/Him" : (this.Pronouns.ToString().Equals("She") ? "She/Her" : "They/Them"), this.City, this.Country, this.Description);
+        }
+    }
+}
---
+++ DatingApp/DatingApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DatingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to our dating app! Please create a profile.");
+
+                //get user input
+                Console.WriteLine("\nEnter your name: ");
+                string name = Console.ReadLine();
+
+                int age;
+                while (true)
+                {
+                    Console.WriteLine("\nEnter your age: ");
+                    if (!int.TryParse(Console.ReadLine(), out age))
+                    {
+                        Console.WriteLine("\nOnly digits from 0-9 allowed, please try again.");
+                        continue;
+                    }
+                    break;
+                }
+
+                Console.WriteLine("\nEnter your pronouns (He, She or They): ");
+                string pronoun = Console.ReadLine();
+
+                Console.WriteLine("\nEnter your City: ");
+                string city = Console.ReadLine();
+
+                Console.WriteLine("\nEnter your Country: ");
+                string country = Console.ReadLine();
+
+                Console.WriteLine("\nEnter a brief description of yourself: ");
+                string description = Console.ReadLine();
+
+                Profile userProfile = new Profile(name, age, pronoun, city, country, description);
+
+                Console.WriteLine("\n" + userProfile.ToString());
+
+            }
+            catch (InvalidDataException exc)
+            {
+                Console.WriteLine("\n" + exc.Message);
+            }
+        }
+    }
+}
+
+
+/*
+ INITIAL PROJECT REQUIREMENTS:
+
+Add the following fields to Profile:
+
+    - string name
+    -  int age
+    - string city
+    - string country
+    - string pronouns
+    - string[] hobbies
+
+We could implement these as properties, but we’ll use fields. Properties are used to:
+
+    - validate values
+    - control external access
+
+Later in this project you’ll see how we achieve the same result with methods.
+
+
+
+
+
+
+*** IMPROVEMENTS ***
+FIXME: instead of having setters throw an exception and displaying one error at a time to the user, add error message to an ArrayList and display content to the user all at once.
+ */
